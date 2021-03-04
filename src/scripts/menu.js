3e6513515290b7e857245574c216fa27694f544b
Ilya <verron228@mail.ru>
2021-03-04 13:01:10 +0300

    v3

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,14 +1,20 @@
 const menuBurger = document.querySelector('.top-bar-burger');
 const menu = document.querySelector('.menu');
+const menuLink = document.querySelectorAll('.menu-item-link');
+console.log(menuLink);
 
-
-menuBurger.addEventListener('click', openMenu)
+menuBurger.addEventListener('click', openMenu);
+menuLink.forEach(link => link.addEventListener('click', closeMenu))
 
 function openMenu() {
 menuBurger.classList.toggle('menu-open');
 menu.classList.toggle('menu-open');
 }
 
+function closeMenu() {
+    menuBurger.classList.remove('menu-open');
+    menu.classList.remove('menu-open');
+}
 
 
 
